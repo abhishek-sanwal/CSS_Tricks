e4354e4371d61cf36bab68b49d0d391b163bda13
Abhishek Sanwal <abhisheksanwal745@gmail.com>
2024-09-08 17:50:04 +0530

    Completed faq accordian section

---
+++ app.js
@@ -0,0 +1,73 @@
+// Javascript
+
+"using strict";
+
+let previouslyClickedButton = null;
+
+const showFaq = (event) => {
+  event.stopPropagation();
+  // Get the current button
+  const currentButton = event.currentTarget;
+
+  // Handle the flag to expand or collapse
+  handleFaq(currentButton, previouslyClickedButton === currentButton);
+};
+
+const handleFaq = (button, isSameButton) => {
+  if (isSameButton) {
+    // Collapse the current div
+    button.classList.toggle("btn");
+    const paragraphs = button.querySelectorAll("p");
+    const images = button.querySelectorAll("img");
+    handleDiv(paragraphs, images, false);
+    previouslyClickedButton = null;
+  } else {
+    // Close previously expanded div
+    if (previouslyClickedButton !== null) {
+      const prevParagraphs = previouslyClickedButton.querySelectorAll("p");
+      const prevImages = previouslyClickedButton.querySelectorAll("img");
+      previouslyClickedButton.classList.toggle("btn");
+      handleDiv(prevParagraphs, prevImages, false);
+    }
+    // Expand the current div
+    const paragraphs = button.querySelectorAll("p");
+    const images = button.querySelectorAll("img");
+    handleDiv(paragraphs, images, true);
+    button.classList.toggle("btn");
+    previouslyClickedButton = button;
+  }
+};
+
+const handleDiv = (paragraphs, images, flag) => {
+  handleParagraphs(paragraphs, flag);
+  handleImages(images, flag);
+};
+
+const handleParagraphs = (paras, flag) => {
+  // flag True means Expand the div
+  // Flag false means Collapse the div
+  if (paras.length > 1) {
+    paras[1].classList.toggle("data-hide", !flag);
+  }
+};
+
+const handleImages = (images, flag) => {
+  // flag true means expand means change pics
+  // flag false means collapse
+  if (images.length > 0) images[0].classList.toggle("img-rotate", !flag);
+};
+
+const changeOpacticy = (event) => {
+  console.log("I am working.");
+  const element = event.currentTarget.querySelector("img");
+  element.dispatchEvent(new Event("mouseover"));
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  const nodes = document.querySelectorAll(".element-button");
+
+  nodes.forEach((node) => {
+    node.addEventListener("click", showFaq);
+    node.addEventListener("mouseover", changeOpacticy);
+  });
+});
